a938d5b96a0e14d246e35be220bb9fa2d3dad8cd
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-13 17:33:12 +1100

    Add search flights feature

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,18 @@
+class FlightsController < ApplicationController
+    def index
+        @departure_airport_options = Airport.all.map{|a| [a.name, a.id]}.unshift(["-", ""])
+        @arrival_airport_options = Airport.all.map{|a| [a.name, a.id]}.unshift(["-", ""])
+        @departure_time_options = Flight.all.select(:departure_time).order(:departure_time).map{|f| [f.departure_time.strftime("%m/%d/%Y"), f.departure_time]}.uniq{|t| t[0]}.unshift(["-", ""])
+        @flights = Flight.all.order(:departure_time)
+        if params[:departure_airport_id].present?
+            @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+        end
+        if params[:arrival_airport_id].present?
+            @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+        end
+        if params[:departure_time].present?
+            departure_time = params[:departure_time].to_date.beginning_of_day
+            @flights = @flights.where(departure_time: departure_time..departure_time.end_of_day)
+        end
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,19 @@ require 'faker'
 
 # Airport.create([{name: "SGN"},  {name: "DAD"}, {name: "MEL"}, {name: "HAN"}, {name: "HKG"}])
 
+
+# Airport.offset(5).destroy_all
+# Flight.destroy_all
 10.times do
     departure_airport = Airport.offset(rand(Airport.count)).first
     arrival_airport = Airport.offset(rand(Airport.count)).first
     while arrival_airport == departure_airport
         arrival_airport = Airport.offset(rand(Airport.count)).first
     end
-    departure_time = Faker::Time.between(from: DateTime.now, to: DateTime.now + 1)
+    departure_time = Faker::Time.between(from: DateTime.now, to: DateTime.now + 31, format: :long)
   
     Flight.create(departure_airport: departure_airport,
                   arrival_airport: arrival_airport,
                   departure_time: departure_time,
                   flight_duration: rand(3..12)*60)
 end
-
-# Airport.offset(5).destroy_all
-# Flight.offset(10).destroy_all
