5ec7263984e89510000a7958a17a2bf01ad38acc
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-13 12:59:32 +1100

    Create airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create([{name: "SGN"},  {name: "DAD"}, {name: "MEL"}, {name: "HAN"}, {name: "HKG"}])
